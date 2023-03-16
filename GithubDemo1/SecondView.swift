825d22442fe4ee50db7428a29fa9ea45cd5b99ff
Jiwowowow <wldn6809@gmail.com>
2023-03-16 18:45:24 +0900

    Create SecondView.

---
+++ GithubDemo1/SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  GithubDemo1
+//
+//  Created by 황지우 on 2023/03/16.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
